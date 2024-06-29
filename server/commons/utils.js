3005ea8a7ea5deec21ae57d24715932912932dec
jh.jun <cnova0713@vadaro.co.kr>
2024-06-30 05:03:48 +0900

    aes ecb & cbc 암호화 및 복호화 함수 추가

--- server/commons/utils.js
+++ server/commons/utils.js
@@ -1,448 +1,439 @@
-const crypto = require('crypto')
+const crypto = require('crypto');
 const numeral = require('numeral');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 module.exports = {
-
-    hasKey :function(_object, key) {
-
+    hasKey: function (_object, key) {
         if (_object.hasOwnProperty) {
-            return _object.hasOwnProperty(key)
+            return _object.hasOwnProperty(key);
         }
-    
-        return false
+
+        return false;
     },
-    hasKeys : function(_object, ...keys) {
-    
-        let res = true
+    hasKeys: function (_object, ...keys) {
+        let res = true;
 
         if (!_object.hasOwnProperty) {
-            res = false
-            return res
+            res = false;
+            return res;
         }
-    
-        for(let i=0; i < keys.length; i++) {
-    
+
+        for (let i = 0; i < keys.length; i++) {
             if (this.hasKey(_object, keys[i])) {
-                continue
+                continue;
             }
-    
-            res = false
-            break
+
+            res = false;
+            break;
         }
-    
-        return res
+
+        return res;
     },
-    hasKeysArray : function(_object, keyArr) {
-    
-        let res = true
-    
+    hasKeysArray: function (_object, keyArr) {
+        let res = true;
+
         if (!_object.hasOwnProperty) {
-            res = false
-            return res
+            res = false;
+            return res;
         }
-    
-        for(let i=0; i < keyArr.length; i++) {
-    
+
+        for (let i = 0; i < keyArr.length; i++) {
             if (this.hasKey(_object, keyArr[i])) {
-                continue
+                continue;
             }
-    
-            res = false
-            break
+
+            res = false;
+            break;
         }
-    
-        return res
+
+        return res;
     },
     // 문자 공백 체크
-    isBlank:function(str) {
-    
+    isBlank: function (str) {
         /*// 문자가 아닐 경우
         if (typeof str !== 'string') {
             return true
         }
         */
-    
+
         if (!str) {
             return true;
         }
-    
+
         // 공백일 경우
         if (String(str).trim().length < 1) {
-            return true
+            return true;
         }
-    
-        return false
+
+        return false;
     },
-    isBlanks :function(...items) {
-    
-        let res = false
-    
-        for(let i=0; i < items.length; i++) {
+    isBlanks: function (...items) {
+        let res = false;
+
+        for (let i = 0; i < items.length; i++) {
             if (this.isBlank(items[i])) {
-                res = true
-                break
+                res = true;
+                break;
             }
         }
-    
-        return res
+
+        return res;
     },
-    isBlanksArray : function(itemArr) {
-    
-        let res = false
-    
+    isBlanksArray: function (itemArr) {
+        let res = false;
+
         if (!Array.isArray(itemArr)) {
-            return true
+            return true;
         }
-    
-        for(let i=0; i < itemArr.length; i++) {
+
+        for (let i = 0; i < itemArr.length; i++) {
             if (this.isBlank(itemArr[i])) {
-                res = true
-                break
+                res = true;
+                break;
             }
         }
-    
-        return res
+
+        return res;
     },
     // 각 객체에서의 문자 공백 체크
-    isObjectBlank : function(_object, ...keys) {
-    
-        let res = false
-    
-        for ( let i=0, e=keys.length; i<e; i++ ) {
-    
+    isObjectBlank: function (_object, ...keys) {
+        let res = false;
+
+        for (let i = 0, e = keys.length; i < e; i++) {
             if (this.isBlank(_object[keys[i]])) {
-                res = true
+                res = true;
                 break;
             }
         }
-    
-        return res
+
+        return res;
     },
-    isObjectBlankArray : function(_object, keysArr, length) {
-    
-        let res = false
-    
+    isObjectBlankArray: function (_object, keysArr, length) {
+        let res = false;
+
         if (!Array.isArray(keysArr)) {
-            return true
+            return true;
         }
-    
-        for (let i =0; i < length; i++ ) {
-    
+
+        for (let i = 0; i < length; i++) {
             console.log(_object[keysArr[i]]);
-    
+
             if (this.isBlank(_object[keysArr[i]])) {
-                res = true
+                res = true;
                 break;
             }
         }
-    
-        return res
+
+        return res;
     },
     // 객체 속 null 체크
-    isNull : function(_object, ...keys) {
-    
-        let res = false
-    
-        for( let i=0, e=keys.length; i<e; i++ ) {
-    
+    isNull: function (_object, ...keys) {
+        let res = false;
+
+        for (let i = 0, e = keys.length; i < e; i++) {
             if (!this.hasKey(_object, keys[i])) {
-                res = true
-                break
+                res = true;
+                break;
             }
-    
+
             if (typeof _object[keys[i]] === 'string' && this.isBlank(_object[keys[i]])) {
                 // 문자 타입 일때만 공백 체크
-                res = true
-                break
+                res = true;
+                break;
             }
         }
-    
-        return res
+
+        return res;
     },
-    isNumCheck : function(_object, ...keys){
-    
-        let res = false
-    
-        for(let i=0, e=keys.length; i<e; i++){
-    
-            if(!this.hasKey(_object,keys[i])){
-                res = true
-                break
+    isNumCheck: function (_object, ...keys) {
+        let res = false;
+
+        for (let i = 0, e = keys.length; i < e; i++) {
+            if (!this.hasKey(_object, keys[i])) {
+                res = true;
+                break;
             }
-    
-            if(typeof _object[keys[i]] === 'string' && this.isBlank(_object[keys[i]])) {
-                res = true
-                break
+
+            if (typeof _object[keys[i]] === 'string' && this.isBlank(_object[keys[i]])) {
+                res = true;
+                break;
             }
-    
-            if(isNaN(parseInt(_object[keys[i]]))){
-                res = true
-                break
+
+            if (isNaN(parseInt(_object[keys[i]]))) {
+                res = true;
+                break;
             }
-    
         }
-        return res
+        return res;
     },
-    isNum : function(item) {
-    
-        return !isNaN(Number(item))
+    isNum: function (item) {
+        return !isNaN(Number(item));
     },
-    isNums : function(...items) {
-    
-        let res = true
-    
-        for( let i=0, e=items.length; i<e; i++ ) {
+    isNums: function (...items) {
+        let res = true;
+
+        for (let i = 0, e = items.length; i < e; i++) {
             if (this.isNum(items[i])) {
-                continue
+                continue;
             }
-    
-            res = false
-            break
+
+            res = false;
+            break;
         }
-    
-        return res
+
+        return res;
     },
-    isBeyondZero :function(item) {
-    
+    isBeyondZero: function (item) {
         return !(isNaN(Number(item)) || Number(item) < 1);
     },
-    areBeyondZero : function(...items) {
-    
-        let res = false
-    
-        for(let i=0; i < items.length; i++ ) {
-    
+    areBeyondZero: function (...items) {
+        let res = false;
+
+        for (let i = 0; i < items.length; i++) {
             if (this.isBeyondZero(items[i])) {
-                res = true
-                continue
+                res = true;
+                continue;
             }
-    
-            res = false
+
+            res = false;
             break;
         }
-    
-        return res
+
+        return res;
     },
-    areBeyondZeroArr : function(arr) {
-    
-        let res = true
-    
+    areBeyondZeroArr: function (arr) {
+        let res = true;
+
         // console.log(Array.isArray(arr));
         if (!Array.isArray(arr)) {
-            return false
+            return false;
         }
-    
-        for(let i=0; i < arr.length; i++ ) {
-    
+
+        for (let i = 0; i < arr.length; i++) {
             if (this.isBeyondZero(arr[i])) {
-                res = true
-                continue
+                res = true;
+                continue;
             }
-    
-            res = false
+
+            res = false;
             break;
         }
-    
-        return res
+
+        return res;
     },
-    isBeyondMinus : function(item) {
-    
+    isBeyondMinus: function (item) {
         return !(isNaN(Number(item)) || Number(item) < 0);
-    },    
-    areBeyondMinus :function(...items) {
-    
-        let res = false
-    
-        for(let i=0; i < items.length; i++ ) {
-    
+    },
+    areBeyondMinus: function (...items) {
+        let res = false;
+
+        for (let i = 0; i < items.length; i++) {
             if (this.isBeyondZero(items[i])) {
-                res = true
-                continue
+                res = true;
+                continue;
             }
-    
-            res = false
+
+            res = false;
             break;
         }
-    
-        return res
+
+        return res;
     },
-    encryptSha256 :function(plainText) {
-    
+    encryptSha256: function (plainText) {
         if (!plainText || typeof plainText !== 'string' || plainText.trim().length < 1) {
-            return false
+            return false;
         }
-        return crypto.createHash('sha256').update(plainText).digest('hex')
+        return crypto.createHash('sha256').update(plainText).digest('hex');
     },
-    secToHMS : (seconds) => {
-    
-        let hour = parseInt(seconds/3600);
-        let min = parseInt((seconds%3600)/60);
-        let sec = parseInt(seconds%60);
-    
-        hour = String(hour).length > 1 ? hour : "0" + hour;
-        min = String(min).length > 1 ? min : "0" + min;
-        sec = String(sec).length > 1 ? sec : "0" + sec;
-    
+    encryptAES_ECB: (plaintext, secretKey) => {
+        const algorithm = 'aes-128-ecb';
+
+        const cipher = crypto.createCipheriv(algorithm, Buffer.from(secretKey), null);
+        let encrypted = cipher.update(plaintext);
+        encrypted = Buffer.concat([encrypted, cipher.final()]).toString('hex');
+
+        return encrypted;
+    },
+    decryptAES_ECB: (encryptText, secretKey) => {
+        const algorithm = 'aes-128-ecb';
+
+        const decipher = crypto.createDecipheriv(algorithm, Buffer.from(secretKey), null);
+        let decrypted = decipher.update(Buffer.from(encryptText, 'hex'));
+        decrypted = Buffer.concat([decrypted, decipher.final()]).toString();
+
+        return decrypted;
+    },
+    encryptAES_CBC: (plaintext, secretKey, ivHex) => {
+        const algorithm = 'aes-128-cbc';
+
+        if (!ivHex) {
+            let iv = crypto.randomBytes(16);
+            ivHex = iv.toString('hex');
+        }
+
+        const cipher = crypto.createCipheriv(algorithm, Buffer.from(secretKey), Buffer.from(ivHex, 'hex'));
+        let encrypted = cipher.update(plaintext);
+        encrypted = Buffer.concat([encrypted, cipher.final()]).toString('hex');
+
+        return { encrypted, ivHex };
+        // return encrypted + ':' + ivHex;
+    },
+    decryptAES_CBC: (encryptText, secretKey, ivHex) => {
+        const algorithm = 'aes-128-cbc';
+
+        const decipher = crypto.createDecipheriv(algorithm, Buffer.from(secretKey), Buffer.from(ivHex, 'hex'));
+        let decrypted = decipher.update(Buffer.from(encryptText, 'hex'));
+        decrypted = Buffer.concat([decrypted, decipher.final()]).toString();
+
+        return decrypted;
+    },
+    secToHMS: (seconds) => {
+        let hour = parseInt(seconds / 3600);
+        let min = parseInt((seconds % 3600) / 60);
+        let sec = parseInt(seconds % 60);
+
+        hour = String(hour).length > 1 ? hour : '0' + hour;
+        min = String(min).length > 1 ? min : '0' + min;
+        sec = String(sec).length > 1 ? sec : '0' + sec;
+
         if (parseInt(hour) < 1) {
             return `${min}:${sec}`;
         }
-    
+
         return `${hour}:${min}:${sec}`;
     },
-    createImgDownPath : (_id) => {
-    
+    createImgDownPath: (_id) => {
         let host = process.env.HOST;
         let port = process.env.PORT;
-        host = "";
-    
+        host = '';
+
         // console.log("img path",_id)
-    
+
         // return `${host}:${port}/api/file/image/${_id}`
         return _id ? `${host}/api/v1/file/image/${_id}` : null;
     },
-    createFileDownPath :(_id) => {
-    
+    createFileDownPath: (_id) => {
         let host = process.env.HOST;
         let port = process.env.PORT;
-    
-        host = "";
+
+        host = '';
         // return `${host}:${port}/api/file/image/${_id}`
         return _id ? `${host}/api/v1/file/download/${_id}` : null;
     },
-    createVideoDownPath :(_id) => {
-    
+    createVideoDownPath: (_id) => {
         let host = process.env.HOST;
         let port = process.env.PORT;
-        host = "";
+        host = '';
         // host = process.env.IS_AWS ? process.env.AWS_HLS_URL: "";
-    
+
         return _id ? `${host}/hls/${_id}/${_id}.m3u8` : null;
     },
-    insertChkFromDB :_result => (_result['success'] && _result['rows']['insertId'] > 0),
-    updateChkFromDB : _result => (_result['success'] && _result['rows']['affectedRows'] > 0),
-    selectChkFromDB : _result => {
-    
+    insertChkFromDB: (_result) => _result['success'] && _result['rows']['insertId'] > 0,
+    updateChkFromDB: (_result) => _result['success'] && _result['rows']['affectedRows'] > 0,
+    selectChkFromDB: (_result) => {
         // sql err
         if (!_result['success']) {
             return -1;
         }
-    
+
         // empty data
         if (_result['rows'].length < 1) {
             return 0;
         }
-    
+
         return 1;
     },
-    duplicateSelectChkFromDB : (_result) => {
-    
+    duplicateSelectChkFromDB: (_result) => {
         // sql err
         if (!_result['success'] || _result['rows'].length < 1) {
             return -1;
         }
-    
-        return _result['rows'][0]["cnt"] > 0 ? 1 : 0;
-    },    
-    searchKeywordsToArray : (str) => {
-    
+
+        return _result['rows'][0]['cnt'] > 0 ? 1 : 0;
+    },
+    searchKeywordsToArray: (str) => {
         return str.trim().split(/(?:\s)+|\s{2,}/gi);
     },
-    createConditionQryForSearch : (column, operator, keywords, frontCondition) => {
-    
+    createConditionQryForSearch: (column, operator, keywords, frontCondition) => {
         let keywordsArr = searchKeywordsToArray(keywords);
-        let condition = !frontCondition ? "where" : frontCondition;
-    
-        let qry = "";
+        let condition = !frontCondition ? 'where' : frontCondition;
+
+        let qry = '';
         let i = 0;
-    
+
         for (let item of keywordsArr) {
-    
             if (i === 0) {
-                qry = ` ${condition} ${column} like '%${item}%' `
-            }
-            else {
-                let temp = ` ${operator} ${column} like '%${item}%' `
-    
+                qry = ` ${condition} ${column} like '%${item}%' `;
+            } else {
+                let temp = ` ${operator} ${column} like '%${item}%' `;
+
                 qry += temp;
             }
             i++;
         }
-    
+
         return qry;
     },
-    commaMoney : (num) => {
-    
+    commaMoney: (num) => {
         return numeral(num).format('0,0');
     },
-    isHereSpecialCharacter : (str) => {
-    
-        let reg = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi
-    
+    isHereSpecialCharacter: (str) => {
+        let reg = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi;
+
         return reg.test(str);
     },
-    chkSpecialCharacter : (str) => {
-    
-        let reg = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi
-    
+    chkSpecialCharacter: (str) => {
+        let reg = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi;
+
         //특수문자 검증
-        if(reg.test(str)){
+        if (reg.test(str)) {
             //특수문자 제거후 리턴
-            return str.replace(reg, "");
+            return str.replace(reg, '');
         } else {
             //특수문자가 없으므로 본래 문자 리턴
             return str;
         }
     },
-    isMaxLength : function(max, str){
-    
+    isMaxLength: function (max, str) {
         if (!str) {
             return true;
         }
-    
+
         // 공백일 경우
         if (String(str).trim().length < 1) {
-            return true
+            return true;
         }
-    
+
         return str.length > max;
     },
-    areMaxLength : function(max, ...items) {
-    
-        let res = false
-    
-        for(let i=0; i < items.length; i++) {
-            if (this.isMaxLength(max,items[i])) {
-                res = true
-                break
+    areMaxLength: function (max, ...items) {
+        let res = false;
+
+        for (let i = 0; i < items.length; i++) {
+            if (this.isMaxLength(max, items[i])) {
+                res = true;
+                break;
             }
         }
-    
-        return res
+
+        return res;
     },
-    areMaxLengthArr : function(max, arr) {
-    
-        let res = false
-    
+    areMaxLengthArr: function (max, arr) {
+        let res = false;
+
         if (!Array.isArray(arr)) {
-            return false
+            return false;
         }
-    
-        for(let i=0; i < arr.length; i++) {
-    
+
+        for (let i = 0; i < arr.length; i++) {
             if (this.isMaxLength(max, arr[i])) {
-                res = true
-                break
+                res = true;
+                break;
             }
         }
-    
-        return res
-    }
-}
 
-
-
-
-
+        return res;
+    },
+};
---
+++ test.js
@@ -0,0 +1,110 @@
+const crypto = require('crypto');
+
+// let key = '1231fsdfsdfsfsdf';
+
+// function encryptAES_CBC(plaintext, key) {
+//     // let key16 = Buffer.from(key); // 16바이트 길이의 키
+//     let iv = crypto.randomBytes(16); // 랜덤 IV 생성
+
+//     console.log('iv', iv);
+//     // console.log('key16', key16);
+
+//     let cipher = crypto.createCipheriv('aes-128-cbc', key, iv);
+
+//     console.log('cipher', cipher);
+
+//     let encrypted = cipher.update(plaintext, 'utf8', 'base64');
+//     encrypted += cipher.final('base64');
+
+//     return iv.toString('base64') + ':' + encrypted;
+// }
+
+// function decryptAES_CBC(encryptText, key) {
+//     // let key16 = Buffer.from(key); // 16바이트 길이의 키
+//     let encryptTextBuffer = Buffer.from(encryptText, 'base64');
+//     console.log('key', key);
+
+//     let iv = encryptTextBuffer.slice(0, 16);
+
+//     console.log('iv', iv);
+
+//     let encryptedText = encryptTextBuffer.slice(16).toString('base64');
+
+//     let decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
+
+//     console.log('decipher', decipher);
+
+//     let decrypted = decipher.update(Buffer.from(encryptedText, 'base64'), 'base64', 'utf8');
+
+//     decrypted += decipher.final('utf8');
+
+//     return decrypted;
+// }
+
+// const crypto = require('crypto');
+
+// const algorithm = 'aes-128-cbc';
+const secretKey = 'abcdefghijklmnop';
+// const iv = '00000000000000000000000000000000';
+
+// let iv = crypto.randomBytes(16).toString('base64');
+// console.log('iv', iv);
+
+const encryptAES_ECB = (plaintext, secretKey) => {
+    const algorithm = 'aes-128-ecb';
+
+    const cipher = crypto.createCipheriv(algorithm, Buffer.from(secretKey), null);
+    let encrypted = cipher.update(plaintext);
+    encrypted = Buffer.concat([encrypted, cipher.final()]).toString('hex');
+
+    return encrypted;
+};
+
+const decryptAES_ECB = (encryptText, secretKey) => {
+    const algorithm = 'aes-128-ecb';
+
+    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(secretKey), null);
+    let decrypted = decipher.update(Buffer.from(encryptText, 'hex'));
+    decrypted = Buffer.concat([decrypted, decipher.final()]).toString();
+
+    return decrypted;
+};
+
+const encryptAES_CBC = (plaintext, secretKey, ivHex) => {
+    const algorithm = 'aes-128-cbc';
+
+    if (!ivHex) {
+        let iv = crypto.randomBytes(16);
+        ivHex = iv.toString('hex');
+    }
+
+    const cipher = crypto.createCipheriv(algorithm, Buffer.from(secretKey), Buffer.from(ivHex, 'hex'));
+    let encrypted = cipher.update(plaintext);
+    encrypted = Buffer.concat([encrypted, cipher.final()]).toString('hex');
+
+    return { encrypted, ivHex };
+    // return encrypted + ':' + ivHex;
+};
+
+const decryptAES_CBC = (encryptText, secretKey, ivHex) => {
+    const algorithm = 'aes-128-cbc';
+
+    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(secretKey), Buffer.from(ivHex, 'hex'));
+    let decrypted = decipher.update(Buffer.from(encryptText, 'hex'));
+    decrypted = Buffer.concat([decrypted, decipher.final()]).toString();
+
+    return decrypted;
+};
+
+const encrypted = encryptAES_ECB('Hello Worlfsdfsddddddddddddddddfd!', secretKey);
+console.log(`encrypted:`, encrypted);
+
+const decrypted = decryptAES_ECB(encrypted, secretKey);
+console.log(`decrypted: ${decrypted}`);
+
+// const encrypted = encryptAES_CBC('Hello World!', secretKey);
+// console.log(`encrypted:`, encrypted);
+// console.log(`encrypted:`, `${encrypted.encrypted}:${encrypted.ivHex}`);
+
+// const decrypted = decryptAES_CBC(encrypted.encrypted, secretKey, encrypted.ivHex);
+// console.log(`decrypted: ${decrypted}`);
